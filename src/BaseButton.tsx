3311d66be4296228944e26a7b5cfe8068b60f45a
Feodor <fdomanov@inbox.ru>
2023-06-03 12:07:10 +0300

    button & input на реакте

--- src/BaseButton.tsx
+++ src/BaseButton.tsx
@@ -1,6 +1,11 @@
 import './css/baseButton.css'
 
-export default function BaseButton({text, color}) {
+interface BaseButtonProps{
+    text:string 
+    color:"green"|"yellow"
+}
+
+export default function BaseButton({text, color}: BaseButtonProps) {
     const colorClass = 'button--' + color
 
     return (
---
+++ src/BaseInput.tsx
@@ -0,0 +1,15 @@
+import './css/baseInput.css'
+
+interface BaseInputProps {
+    placeholder: string 
+}
+
+export default function BaseInput({placeholder}: BaseInputProps) {
+
+    return (
+        <>
+            <input type="text" className="input" placeholder={placeholder} />
+            
+        </>
+    )
+}
