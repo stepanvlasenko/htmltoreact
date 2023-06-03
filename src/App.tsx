a4629ff14b443c6ca586ed854143733d120c699a
Stepan Vlasenko <Reyden732@gmail.com>
2023-06-03 13:48:24 +0300

    move font.css to app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import BaseButton from "./BaseButton"
-import BaseInput from "./BaseInput"
+import BaseButton from "./components/BaseButton"
+import BaseInput from "./components/BaseInput"
+import Post from "./components/Post"
 
-import './css/font.css'
+import './app.css'
+
+const testPost = {
+    id: 1,
+    userId: 1,
+    title: 'test-post',
+    content: 'test-content al la lalalsklfkd',
+    publishDate: '01.06.2006',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png'
+}
 
 export default function App() {
     return (
         <>
             <BaseButton text="text" color="blue"></BaseButton>
             <BaseInput placeholder="abc"></BaseInput> 
+            <div className="test">
+                <Post post={testPost}></Post>
+
+            </div>
         </>
     )
 }
