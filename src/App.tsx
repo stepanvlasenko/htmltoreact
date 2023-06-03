9f049399e9fc1416abb54deca95f8d411a5fc317
stepanvlasenko <reyden732@gmail.com>
2023-06-03 21:54:54 +0300

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -24,8 +24,8 @@ export default function App() {
             }}></BaseInput> 
             <div className="test">
                 <Post post={testPost}></Post>
-
             </div>
+            
         </>
     )
 }
